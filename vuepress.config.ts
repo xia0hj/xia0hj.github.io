5945c43937fd2f4ba88074e18be9deb2ed7a7dc2
xia0hj <719887163@qq.com>
2022-07-17 20:02:03 +0800

    docs: add previous interview notes.

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,7 +1,50 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点'
+  description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    navbar: [
+      { text: 'Home', link: '/' },
+      { text: '面试', link: '/interview/' },
+      { text: 'My Github', link: 'https://github.com/xia0hj' }
+    ],
+    sidebar: {
+      '/interview': [
+        {
+          text: 'Vue',
+          link: '/interview/vue.md'
+        },
+        {
+          text: 'JavaScript',
+          link: '/interview/javascript.md'
+        },
+        {
+          text: 'CSS',
+          link: '/interview/css.md'
+        },
+        {
+          text: '计算机网络',
+          link: '/interview/network.md'
+        },
+        {
+          text: 'React',
+          link: '/interview/react.md'
+        },
+        {
+          text: '前端工具链',
+          link: '/interview/toolchain.md'
+        },
+        {
+          text: '后端相关',
+          link: '/interview/backend.md'
+        },
+        {
+          text: '职业规划',
+          link: '/interview/career.md'
+        },
+      ]
+    }
+  })
 })
